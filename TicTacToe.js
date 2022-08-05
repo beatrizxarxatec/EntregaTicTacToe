4e1e9df128b742c0fa9a830798f0b0c1b685c617
beatrizxarxatec <beatriz.rodriguez@xarxatecactiva.com>
2022-08-05 18:29:32 +0200

    Actualizado

--- TicTacToe.js
+++ TicTacToe.js
@@ -2,29 +2,58 @@ const estado = ["b", "b", "b", "b", "b", "b", "b", "b", "b"];
 let jugadorActual = "c";
 let seEstaJugando = false;
 
+function Resaltar(idImagen){
+    document.getElementById(idImagen).classList.remove("sinResaltado");
+    document.getElementById(idImagen).classList.add("resaltado");
+}
+
 function VerSiGanador(valor) {
     if (estado[0] == valor && estado[1] == valor && estado[2] == valor) {
+        Resaltar(0);
+        Resaltar(1);
+        Resaltar(2);
         return true;
     }
     if (estado[3] == valor && estado[4] == valor && estado[5] == valor) {
+        Resaltar(3);
+        Resaltar(4);
+        Resaltar(5);
         return true;
     }
     if (estado[6] == valor && estado[7] == valor && estado[8] == valor) {
+        Resaltar(6);
+        Resaltar(7);
+        Resaltar(8);
         return true;
     }
     if (estado[0] == valor && estado[3] == valor && estado[6] == valor) {
+        Resaltar(0);
+        Resaltar(3);
+        Resaltar(6);
         return true;
     }
     if (estado[1] == valor && estado[4] == valor && estado[7] == valor) {
+        Resaltar(1);
+        Resaltar(4);
+        Resaltar(7);
         return true;
     }
     if (estado[2] == valor && estado[5] == valor && estado[8] == valor) {
+        Resaltar(2);
+        Resaltar(5);
+        Resaltar(8);
         return true;
     }
     if (estado[0] == valor && estado[4] == valor && estado[8] == valor) {
+        Resaltar(0);
+        Resaltar(4);
+        Resaltar(8);
         return true;
     }
     if (estado[2] == valor && estado[4] == valor && estado[6] == valor) {
+        Resaltar(2);
+        Resaltar(4);
+        Resaltar(6);
         return true;
     }
     return false;
@@ -49,24 +78,24 @@ function ClickEnImagen(idImagen) {
             estado[idImagen] = "c";
             document.getElementById(idImagen).src = "imagenCero.jpg";
         }
-        else if (estado[idImagen] == "b" && jugadorActual == "x") {
+        if (estado[idImagen] == "b" && jugadorActual == "x") {
             estado[idImagen] = "x";
             document.getElementById(idImagen).src = "imagenX.jpg";
         }
         const esGanadorCero = VerSiGanador("c");
         if (esGanadorCero) {
             seEstaJugando = false;
-            alert("Es ganador Cero");
+            setTimeout(()=>alert("Es ganador Cero"));
         }
         const esGanadorX = VerSiGanador("x");
         if (esGanadorX) {
             seEstaJugando = false;
-            alert("Es ganador X");
+            setTimeout(()=>alert("Es ganador X"));
         }
         const todasOcupdas = VerSiTodasOcupadas();
         if (!esGanadorCero && !esGanadorX && todasOcupdas) {
             seEstaJugando = false;
-            alert("No hay ganador");
+            setTimeout(()=>alert("No hay ganador"));
         }
 
         if (jugadorActual == "c") {
@@ -82,6 +111,8 @@ function InicializarEstados() {
     for (let i = 0; i < 9; i++) {
         estado[i] = "b";
         document.getElementById(i).src = "imagenBlanco.jpg";
+        document.getElementById(i).classList.remove("sinResaltado");
+        document.getElementById(i).classList.add("sinResaltado");
     }
     jugadorActual = "c";
     seEstaJugando = true;
